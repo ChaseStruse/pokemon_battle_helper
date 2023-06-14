e5d1e89cc5a8efef0f56abd82721c931ac582ec8
Chase Struse <chasestruse@gmail.ocm>
2023-06-14 09:51:38 -0500

    Added stats

--- src/models/pokemon.py
+++ src/models/pokemon.py
@@ -5,4 +5,4 @@ from dataclasses import dataclass
 class Pokemon:
     name: str
     types: []
-
+    stats: {}
--- src/services/pokemon_api_service.py
+++ src/services/pokemon_api_service.py
@@ -3,14 +3,14 @@ import requests
 from src.models.pokemon import Pokemon
 
 
-class PokemonApiService():
+class PokemonApiService:
 
     def get_pokemon(self, pokemon_name):
         response = requests.get("https://pokeapi.co/api/v2/pokemon/" + pokemon_name).json()
-        pokemon_name = response["name"]
-        pokemon_types = self.get_pokemon_types(response)
-
-        return Pokemon(pokemon_name, pokemon_types)
+        name = response["name"]
+        types = self.get_pokemon_types(response)
+        stats = self.get_base_stats(response)
+        return Pokemon(name, types, stats)
 
     def get_pokemon_types(self, response_json):
         types_array = []
@@ -19,10 +19,22 @@ class PokemonApiService():
 
         return types_array
 
+    def get_base_stats(self, response_json):
+        stats = {}
+        for stat in response_json["stats"]:
+            stats[stat["stat"]["name"]] = stat["base_stat"]
+
+        return stats
+
     def pretty_print_pokemon(self, pokemon):
         print("Name: " + pokemon.name)
         print("Type(s): ")
         for type in pokemon.types:
             print(type)
+
+        print("Stats:")
+        for stat, base_value in pokemon.stats.items():
+            print(f"{stat} : {base_value}")
+
 
 
